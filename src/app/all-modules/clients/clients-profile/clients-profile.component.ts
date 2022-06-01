4ec95a38de0165b380f0f90e05d4a3f6c199a92f
shubh6387 <shubhamknp74@gmail.com>
2022-06-01 10:24:24 +0530

    done

--- src/app/all-modules/clients/clients-profile/clients-profile.component.ts
+++ src/app/all-modules/clients/clients-profile/clients-profile.component.ts
@@ -11,111 +11,111 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ClientsProfileComponent implements OnInit {
   public allClients = [];
-  public client:any;
-   public clientData=[];
-   public allProjects=[];
-   public clientName:any;
+  public client: any;
+  public clientData = [];
+  public allProjects = [];
+  public clientName: any;
   public clientId;
   public adminId;
-  public allTasks=[];
-  public pendingTasks=[];
-  public completedTasks=[];
-  public project:any;
+  public allTasks = [];
+  public pendingTasks = [];
+  public completedTasks = [];
+  public project: any;
   pendingTask: any;
-  
+
   constructor(private allModulesService: AllModulesService,
-    private http:HttpClient,
-              private route: ActivatedRoute) {
-                this.adminId=sessionStorage.getItem("adminId")
-               }
+    private http: HttpClient,
+    private route: ActivatedRoute) {
+    this.adminId = sessionStorage.getItem("adminId")
+  }
 
 
 
 
 
 
-    
+
 
   ngOnInit() {
     this.route.params.pipe(map(id => {
-     
+
       this.clientId = id.id;
-    }), mergeMap(() => this.http.get("http://localhost:8443/admin//clients/getDataClient"+"/"+this.adminId))).subscribe((data:any) => {
+    }), mergeMap(() => this.http.get("http://localhost:8443/admin//clients/getDataClient" + "/" + this.adminId))).subscribe((data: any) => {
       this.allClients = data;
-      
+
       this.clientData = this.allClients.filter(client => client.id == this.clientId);
-     
 
-      this.client=this.clientData[0];
 
-      
+      this.client = this.clientData[0];
+
+
       this.projects();
     });
 
-    
+
 
 
   }
-  projects(){
-       this.http.get("http://localhost:8443/admin/projects/getAdminproject"+"/"+this.adminId).subscribe((res:any)=>{
-         
-         this.allProjects=res;
-        
-         this.project=this.allProjects.filter(name => name.clientId ==  this.clientId)
-         
+  projects() {
+    this.http.get("http://localhost:8443/admin/projects/getAdminproject" + "/" + this.adminId).subscribe((res: any) => {
 
-         this.project.map((item)=>{
-           console.log(item)
-                if(item.lstTasks){
-                  item.lstTasks.map((data)=>{
-                   
-                    this.pendingTasks.push(data);
-                    this.allTasks.push(data)
-                  })
-                }
-                
-                if(item.lstCompleted){
-                  item.lstCompleted.map((data)=>{
-                   
-                    this.completedTasks.push(data);
-                    this.allTasks.push(data)
-                  })
-                }
+      this.allProjects = res;
 
-         if(item. lstProgress){
-          item.lstProgress.map((data)=>{
+      this.project = this.allProjects.filter(name => name.clientId == this.clientId)
+
+
+      this.project.map((item) => {
+        console.log(item)
+        if (item.lstTasks) {
+          item.lstTasks.map((data) => {
+
+            this.pendingTasks.push(data);
+            this.allTasks.push(data)
+          })
+        }
+
+        if (item.lstCompleted) {
+          item.lstCompleted.map((data) => {
+
+            this.completedTasks.push(data);
+            this.allTasks.push(data)
+          })
+        }
+
+        if (item.lstProgress) {
+          item.lstProgress.map((data) => {
             this.allTasks.push(data)
           })
 
-         }
-         if(item. lstInprogress){
-          item.lstInprogress.map((data)=>{
+        }
+        if (item.lstInprogress) {
+          item.lstInprogress.map((data) => {
             this.allTasks.push(data)
           })
 
-         }
-         if(item. lstHold){
-          item.lstHold.map((data)=>{
+        }
+        if (item.lstHold) {
+          item.lstHold.map((data) => {
             this.allTasks.push(data)
           })
 
-         }
-         if(item. lstReview){
-          item.lstReview.map((data)=>{
+        }
+        if (item.lstReview) {
+          item.lstReview.map((data) => {
             this.allTasks.push(data)
           })
 
-         }
-          
-          
-         
+        }
 
-         })
-         
-       })
+
+
+
+      })
+
+    })
   }
 
 
- 
+
 
 }
--- src/app/pages/pricing/pricing.component.ts
+++ src/app/pages/pricing/pricing.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 @Component({
   selector: "app-pricing",
   templateUrl: "./pricing.component.html",
   styleUrls: ["./pricing.component.css"],
 })
 export class PricingComponent implements OnInit {
-
+  check = true
 
   public PtSingleUserMonth: any;
   public PtMultiUserMonth: any;
@@ -38,7 +39,7 @@ export class PricingComponent implements OnInit {
   public dSingleoneMonthAmount: any;
   public dMultioneMonthAmount: any;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
@@ -72,36 +73,54 @@ export class PricingComponent implements OnInit {
       this.epMultiUser = event;
       console.log(this.epMultiUser, "<<<<<<<<<<<<<<>>>>>>>>>>")
       this.epMultioneMonthAmount = (this.epMultiMonthCost * this.epMultiUser)
-      console.log(this.epMultioneMonthAmount, "kkkkllllll")
 
     }
 
 
   }
-  getPlan(val) {
+  getPlan(val, val2) {
     if (val == 'ptSingle') {
+      this.PtSingleUserMonth = val2;
       this.totalPtSingleMonthAmount = (this.ptSingleoneMonthAmount * this.PtSingleUserMonth);
     }
 
     if (val == 'ptMulti') {
+      this.PtMultiUserMonth = val2;
       this.totalPtMultiMonthAmount = (this.ptMultioneMonthAmount * this.PtMultiUserMonth);
 
     }
 
     if (val == 'dSingle') {
+      this.dSingleUserMonth = val2;
       this.totaldSingleMonthAmount = (this.dSingleoneMonthAmount * this.dSingleUserMonth);
 
     }
     if (val == 'dMulti') {
+      this.dMultiUserMonth = val2;
       this.totaldMultiMonthAmount = (this.dMultioneMonthAmount * this.dMultiUserMonth);
     }
     if (val == 'epMulti') {
+      this.epMultiUserMonth = val2;
       this.totalEpMonthAmount = (this.epMultioneMonthAmount * this.epMultiUserMonth);
-      console.log(this.totalEpMonthAmount, "..../..../.")
 
     }
 
   }
-
-
+  getData(val) {
+    if (val == 'dSingle') {
+      this.router.navigate(['/pages/checkout'], { queryParams: { totaldSingleMonthAmount: this.totaldSingleMonthAmount, dSingleUser: this.dSingleUser }, skipLocationChange: true });
+    }
+    if (val == 'dMulti') {
+      this.router.navigate(['/pages/checkout'], { queryParams: { totaldMultiMonthAmount: this.totaldMultiMonthAmount, dMultiUser: this.dMultiUser }, skipLocationChange: true });
+    }
+    if (val == 'epMulti') {
+      this.router.navigate(['/pages/checkout'], { queryParams: { totalEpMonthAmount: this.totalEpMonthAmount, epMultiUser: this.epMultiUser }, skipLocationChange: true });
+    }
+    if (val == 'ptSingle') {
+      this.router.navigate(['/pages/checkout'], { queryParams: { totalPtSingleMonthAmount: this.totalPtSingleMonthAmount, PtSingleUser: this.PtSingleUser }, skipLocationChange: true });
+    }
+    if (val == 'ptMulti') {
+      this.router.navigate(['/pages/checkout'], { queryParams: { totalPtMultiMonthAmount: this.totalPtMultiMonthAmount, PtMultiUser: this.PtMultiUser }, skipLocationChange: true });
+    }
+  }
 }
